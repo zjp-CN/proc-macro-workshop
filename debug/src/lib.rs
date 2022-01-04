55f1ad656b0863541eba48d2ed0e0e926b9cff5e
zjp <jiping_zhou@foxmail.com>
2022-01-05 00:23:00 +0800

    debug test: 02 pass

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,34 @@
 #![allow(unused)]
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+    let token = custom_debug(input);
+    TokenStream::from(token)
+}
 
-    TokenStream::new()
+fn custom_debug(input: DeriveInput) -> proc_macro2::TokenStream {
+    let DeriveInput { attrs, vis, ident, generics, data } = input;
+    if let Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) = &data {
+        let ident_str = ident.to_string();
+        let field_idents = named.iter().map(|f| f.ident.as_ref().unwrap());
+        let field_idents_str = field_idents.clone().map(|i| i.to_string());
+        quote! {
+            impl ::std::fmt::Debug for #ident {
+                fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
+                    f.debug_struct(&#ident_str)
+                        #(
+                            .field(&#field_idents_str, &self.#field_idents)
+                        )*
+                        .finish()
+                }
+            }
+        }
+    } else {
+        unimplemented!()
+    }
 }
