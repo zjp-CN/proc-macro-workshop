05a2d36686f93a2f0243326c9fd98b3410a6e30a
zjp <jiping_zhou@foxmail.com>
2022-01-05 21:28:57 +0800

    debug test: 04 pass

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,9 @@
-#![allow(unused)]
-
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::{format_ident, quote};
+use quote::quote;
 use syn::{
     parse_macro_input, spanned::Spanned, Data, DataStruct, DeriveInput, Error, Fields, FieldsNamed, Ident,
-    Lit, Meta, MetaNameValue, Path, Result, Type,
+    Lit, Meta, MetaNameValue, Result, TypeParam,
 };
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
@@ -18,21 +16,22 @@ pub fn derive(input: TokenStream) -> TokenStream {
     })
 }
 
-fn custom_debug(input: DeriveInput) -> Result<TokenStream2> {
-    let DeriveInput { attrs, vis, ident, generics, data } = &input;
-    if let Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) = data {
+fn custom_debug(mut input: DeriveInput) -> Result<TokenStream2> {
+    if let Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) = &input.data {
+        let (ident, generics) = (&input.ident, &mut input.generics);
         let ident_str = ident.to_string();
         let field_idents = named.iter().map(|f| f.ident.as_ref().unwrap());
         let field_idents_str = field_idents.clone().map(|i| i.to_string());
 
-        let field_rhs = named.iter()
-                             .map(|f| f.attrs.as_slice())
-                             .zip(field_idents)
-                             .map(|(a, i)| attr_debug(a, i).map(|t| t.unwrap_or(quote! {&self.#i})))
-                             .collect::<Result<Vec<_>>>()?;
+        let field_rhs = field_idents.zip(named.iter().map(|f| f.attrs.as_slice()))
+                                    .map(|(i, a)| attr_debug(a, i).map(|t| t.unwrap_or(quote! {&self.#i})))
+                                    .collect::<Result<Vec<_>>>()?;
+
+        generics.type_params_mut().map(generic_add_debug).last();
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
         Ok(quote! {
-            impl ::std::fmt::Debug for #ident {
+            impl #impl_generics ::std::fmt::Debug for #ident #ty_generics #where_clause {
                 fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
                     f.debug_struct(&#ident_str)
                         #(
@@ -53,13 +52,17 @@ fn attr_debug(attrs: &[syn::Attribute], ident: &Ident) -> Result<Option<TokenStr
             Ok(Meta::NameValue(MetaNameValue { path, lit: Lit::Str(s), .. })) if path.is_ident("debug") => {
                 Some(Ok(s))
             }
-            // Err(err) => return Some(Err(Error::new(attr.span(), "failed to parse attr meta"))),
             _ => Some(Err(Error::new(attr.span(), "failed to parse attr meta"))),
         }
     }
     match attrs.iter().find_map(debug) {
         None => Ok(None),
-        Some(Ok(fmt)) => Ok(Some(quote! {&format_args!(#fmt, self.#ident)})),
+        Some(Ok(fmt)) => Ok(Some(quote! {&::std::format_args!(#fmt, self.#ident)})),
         Some(Err(err)) => Err(err),
     }
+}
+
+fn generic_add_debug(ty: &mut TypeParam) {
+    let TypeParam { bounds, .. } = ty;
+    bounds.push(syn::parse_quote!(::std::fmt::Debug));
 }
