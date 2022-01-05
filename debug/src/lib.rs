13aac9611b5453d29f3d532ab32c0c0e9a99a0b7
zjp <jiping_zhou@foxmail.com>
2022-01-05 17:08:58 +0800

    debug test: 03 pass

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,34 +1,65 @@
 #![allow(unused)]
 
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed};
+use syn::{
+    parse_macro_input, spanned::Spanned, Data, DataStruct, DeriveInput, Error, Fields, FieldsNamed, Ident,
+    Lit, Meta, MetaNameValue, Path, Result, Type,
+};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let token = custom_debug(input);
-    TokenStream::from(token)
+
+    TokenStream::from(match custom_debug(input) {
+        Ok(token) => token,
+        Err(err) => err.to_compile_error(),
+    })
 }
 
-fn custom_debug(input: DeriveInput) -> proc_macro2::TokenStream {
-    let DeriveInput { attrs, vis, ident, generics, data } = input;
-    if let Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) = &data {
+fn custom_debug(input: DeriveInput) -> Result<TokenStream2> {
+    let DeriveInput { attrs, vis, ident, generics, data } = &input;
+    if let Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) = data {
         let ident_str = ident.to_string();
         let field_idents = named.iter().map(|f| f.ident.as_ref().unwrap());
         let field_idents_str = field_idents.clone().map(|i| i.to_string());
-        quote! {
+
+        let field_rhs = named.iter()
+                             .map(|f| f.attrs.as_slice())
+                             .zip(field_idents)
+                             .map(|(a, i)| attr_debug(a, i).map(|t| t.unwrap_or(quote! {&self.#i})))
+                             .collect::<Result<Vec<_>>>()?;
+
+        Ok(quote! {
             impl ::std::fmt::Debug for #ident {
                 fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
                     f.debug_struct(&#ident_str)
                         #(
-                            .field(&#field_idents_str, &self.#field_idents)
+                            .field(&#field_idents_str, #field_rhs)
                         )*
                         .finish()
                 }
             }
-        }
+        })
     } else {
-        unimplemented!()
+        Err(Error::new(input.span(), "Named Struct Only :)"))
+    }
+}
+
+fn attr_debug(attrs: &[syn::Attribute], ident: &Ident) -> Result<Option<TokenStream2>> {
+    fn debug(attr: &syn::Attribute) -> Option<Result<syn::LitStr>> {
+        match attr.parse_meta() {
+            Ok(Meta::NameValue(MetaNameValue { path, lit: Lit::Str(s), .. })) if path.is_ident("debug") => {
+                Some(Ok(s))
+            }
+            // Err(err) => return Some(Err(Error::new(attr.span(), "failed to parse attr meta"))),
+            _ => Some(Err(Error::new(attr.span(), "failed to parse attr meta"))),
+        }
+    }
+    match attrs.iter().find_map(debug) {
+        None => Ok(None),
+        Some(Ok(fmt)) => Ok(Some(quote! {&format_args!(#fmt, self.#ident)})),
+        Some(Err(err)) => Err(err),
     }
 }
