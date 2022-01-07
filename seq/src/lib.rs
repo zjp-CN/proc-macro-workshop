bac98fb42c8c0ca6d0e142a3a4c111e94b994292
zjp <jiping_zhou@foxmail.com>
2022-01-07 18:04:18 +0800

    seq 01: pass

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,32 @@
+#![allow(unused)]
+
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{parse::Parse, Ident, Lit, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = syn::parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    TokenStream::new()
+}
+
+struct Seq {
+    ident:      Ident,
+    in_token:   Token![in],
+    lhs:        Lit,
+    dot2_token: Token![..],
+    rhs:        Lit,
+    tokens:     TokenStream2,
+}
+
+impl Parse for Seq {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Seq { ident:      input.parse()?,
+                 in_token:   input.parse()?,
+                 lhs:        input.parse()?,
+                 dot2_token: input.parse()?,
+                 rhs:        input.parse()?,
+                 tokens:     input.parse()?, })
+    }
 }
