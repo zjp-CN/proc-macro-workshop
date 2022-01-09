c77e64d0621d43a45704936a4db025b618ab6d5f
zjp <jiping_zhou@foxmail.com>
2022-01-10 01:33:05 +0800

    sorted 03: pass

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,17 +1,49 @@
+use std::ops::ControlFlow;
+
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
+use syn::{spanned::Spanned, Error};
 
 #[proc_macro_attribute]
 pub fn sorted(arg: TokenStream, input: TokenStream) -> TokenStream {
     use quote::ToTokens;
-    use syn::{parse_macro_input, spanned::Spanned, Error, Item};
+    use syn::{parse_macro_input, Item};
 
-    let arg = TokenStream2::from(arg);
     let input = parse_macro_input!(input as Item);
 
-    if matches!(&input, Item::Enum(_)) {
-        TokenStream::from(input.to_token_stream())
+    if let Item::Enum(input) = &input {
+        if let Err(err) = sort(input) {
+            TokenStream::from(err.to_compile_error())
+        } else {
+            TokenStream::from(input.to_token_stream())
+        }
     } else {
-        TokenStream::from(Error::new(arg.span(), "expected enum or match expression").to_compile_error())
+        let span = TokenStream2::from(arg).span();
+        TokenStream::from(Error::new(span, "expected enum or match expression").to_compile_error())
+    }
+}
+
+fn sort(input: &syn::ItemEnum) -> Result<(), Error> {
+    let input = &input.variants;
+
+    let vars: Vec<_> = input.iter().map(|variant| variant.ident.to_string()).collect();
+    let mut vars_sorted = vars.clone();
+    vars_sorted.sort();
+
+    if let ControlFlow::Break((sorted, raw)) = vars_sorted.iter().zip(vars.iter()).try_for_each(cmp_str) {
+        let pos = vars.iter().position(|i| i == sorted).unwrap(); // 一定能找到
+        Err(Error::new(input[pos].span(), format!("{} should sort before {}", sorted, raw)))
+    } else {
+        Ok(())
+    }
+}
+
+type StrPair<'s> = (&'s String, &'s String);
+
+fn cmp_str((sorted, raw): StrPair) -> ControlFlow<StrPair> {
+    if sorted != raw {
+        ControlFlow::Break((sorted, raw))
+    } else {
+        ControlFlow::Continue(())
     }
 }
