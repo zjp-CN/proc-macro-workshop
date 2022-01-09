c84a79b75357425dfdea96fec8d49a316e5ad9ad
zjp <jiping_zhou@foxmail.com>
2022-01-10 00:05:40 +0800

    sorted 01: pass

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,8 @@
 use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
-
-    unimplemented!()
+pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
+    use quote::ToTokens;
+    let input = syn::parse_macro_input!(input as syn::Item);
+    TokenStream::from(input.to_token_stream())
 }
