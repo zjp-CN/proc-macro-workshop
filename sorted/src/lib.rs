b9526ada09f6c9e297d63172c7f45f1ddbe339e1
zjp <jiping_zhou@foxmail.com>
2022-01-10 00:20:58 +0800

    sorted 02: pass

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,8 +1,17 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 
 #[proc_macro_attribute]
-pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(arg: TokenStream, input: TokenStream) -> TokenStream {
     use quote::ToTokens;
-    let input = syn::parse_macro_input!(input as syn::Item);
-    TokenStream::from(input.to_token_stream())
+    use syn::{parse_macro_input, spanned::Spanned, Error, Item};
+
+    let arg = TokenStream2::from(arg);
+    let input = parse_macro_input!(input as Item);
+
+    if matches!(&input, Item::Enum(_)) {
+        TokenStream::from(input.to_token_stream())
+    } else {
+        TokenStream::from(Error::new(arg.span(), "expected enum or match expression").to_compile_error())
+    }
 }
