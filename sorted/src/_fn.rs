5951b2d93ca0a31ec09ebaf1873a76492dbb62ce
zjp <jiping_zhou@foxmail.com>
2022-01-10 22:42:58 +0800

    sorted 07: pass

--- sorted/src/_fn.rs
+++ sorted/src/_fn.rs
@@ -2,7 +2,7 @@ use std::ops::ControlFlow;
 use syn::{
     spanned::Spanned,
     visit_mut::{self, VisitMut},
-    Pat,
+    Error, Pat, Result,
 };
 
 pub fn process(input: &mut syn::ItemFn) -> proc_macro2::TokenStream {
@@ -40,14 +40,22 @@ impl VisitMut for MatchSorted {
 }
 
 impl MatchSorted {
-    fn to_vec_string(&self) -> impl Iterator<Item = Vec<String>> + '_ {
-        self.0
-            .iter()
-            .map(|match_item| match_item.iter().filter_map(path_to_string).collect())
+    fn to_vec_string(&self) -> Result<Vec<Vec<String>>> {
+        let mut v = Vec::with_capacity(8);
+        for match_item in self.0.iter() {
+            let mut u = Vec::with_capacity(8);
+            for pat in match_item.iter() {
+                u.push(path_to_string(pat)?);
+            }
+            v.push(u);
+        }
+        Ok(v)
     }
 
-    fn cmp(&self) -> syn::Result<()> {
-        if let ControlFlow::Break(err) = self.to_vec_string().zip(self.0.iter()).try_for_each(cmp_str) {
+    fn cmp(&self) -> Result<()> {
+        if let ControlFlow::Break(err) =
+            self.to_vec_string()?.into_iter().zip(self.0.iter()).try_for_each(cmp_str)
+        {
             Err(err)
         } else {
             Ok(())
@@ -56,21 +64,21 @@ impl MatchSorted {
 }
 
 // 只支持在部分模式中取路径
-fn extract_path(pat: &Pat) -> Option<&syn::Path> {
-    Some(match pat {
+fn extract_path(pat: &Pat) -> Result<&syn::Path> {
+    Ok(match pat {
         Pat::Path(path) => &path.path,
         Pat::Struct(s) => &s.path,
         Pat::TupleStruct(s) => &s.path,
-        _ => return None,
+        p => return Err(Error::new(p.span(), "unsupported by #[sorted]")),
     })
 }
 
 // 把每个匹配分支中的路径（包括多路径形式的路径）拼接成一个字符串
-fn path_to_string(pat: &Pat) -> Option<String> {
+fn path_to_string(pat: &Pat) -> Result<String> {
     extract_path(pat).map(|p| p.segments.iter().map(|s| s.ident.to_string()).collect::<Vec<_>>().join("::"))
 }
 
-fn cmp_str((raw, match_item): (Vec<String>, &Vec<Pat>)) -> ControlFlow<syn::Error> {
+fn cmp_str((raw, match_item): (Vec<String>, &Vec<Pat>)) -> ControlFlow<Error> {
     if let Err(err) =
         crate::cmp::StringCmp::new(raw, |pos| extract_path(&match_item[pos]).unwrap().span()).check()
     {
