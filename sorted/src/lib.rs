2def04b89e65cb21d061e4197d26b77319e89c92
zjp <jiping_zhou@foxmail.com>
2022-01-10 13:33:17 +0800

    sorted 04: pass

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -6,20 +6,22 @@ use syn::{spanned::Spanned, Error};
 
 #[proc_macro_attribute]
 pub fn sorted(arg: TokenStream, input: TokenStream) -> TokenStream {
-    use quote::ToTokens;
+    use quote::{quote, ToTokens};
     use syn::{parse_macro_input, Item};
 
     let input = parse_macro_input!(input as Item);
 
     if let Item::Enum(input) = &input {
         if let Err(err) = sort(input) {
-            TokenStream::from(err.to_compile_error())
+            let err = err.to_compile_error();
+            TokenStream::from(quote! { #input #err })
         } else {
             TokenStream::from(input.to_token_stream())
         }
     } else {
         let span = TokenStream2::from(arg).span();
-        TokenStream::from(Error::new(span, "expected enum or match expression").to_compile_error())
+        let err = Error::new(span, "expected enum or match expression").to_compile_error();
+        TokenStream::from(quote! { #input #err })
     }
 }
 
@@ -32,7 +34,7 @@ fn sort(input: &syn::ItemEnum) -> Result<(), Error> {
 
     if let ControlFlow::Break((sorted, raw)) = vars_sorted.iter().zip(vars.iter()).try_for_each(cmp_str) {
         let pos = vars.iter().position(|i| i == sorted).unwrap(); // 一定能找到
-        Err(Error::new(input[pos].span(), format!("{} should sort before {}", sorted, raw)))
+        Err(Error::new(input[pos].ident.span(), format!("{} should sort before {}", sorted, raw)))
     } else {
         Ok(())
     }
