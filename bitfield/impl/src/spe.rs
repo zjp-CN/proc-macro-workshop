802a093b9553a6bed0292d309175f203c8af05a6
zjp <jiping_zhou@foxmail.com>
2022-01-18 19:33:02 +0800

    bitfield 08: pass

--- bitfield/impl/src/spe.rs
+++ bitfield/impl/src/spe.rs
@@ -1,7 +1,12 @@
 pub fn derive_bitfield_specifier_for_enum(input: syn::ItemEnum) -> proc_macro2::TokenStream {
     let enum_name = &input.ident;
     let vars = input.variants.iter().map(|v| &v.ident);
-    let bits = log2(input.variants.len() as u32); // TODO: 考虑 len=0 和不是 8 倍数的情况
+    let bits = if let Some(b) = log2_exact(input.variants.len() as u32) {
+        b
+    } else {
+        return syn::Error::new(proc_macro2::Span::call_site(),
+                               "BitfieldSpecifier expected a number of variants which is a power of 2").to_compile_error();
+    };
     let ty = quote::format_ident!("B{}", bits);
     let ty_u = quote::format_ident!("__{}", input.ident);
     let ty_equiv = quote::format_ident!("__{}Equiv", input.ident);
@@ -38,11 +43,21 @@ pub fn derive_bitfield_specifier_for_enum(input: syn::ItemEnum) -> proc_macro2::
 }
 
 // 改进自 https://users.rust-lang.org/t/logarithm-of-integers/8506/5
+// 这个函数可以根据吗 enum 的成员数自动计算最小 bits
+#[allow(dead_code)]
 const fn log2(n: u32) -> u32 { u32::BITS - n.leading_zeros() - 1 + (n.count_ones() != 1) as u32 }
+
+const fn log2_exact(n: u32) -> Option<u32> {
+    if n.count_ones() == 1 {
+        Some(u32::BITS - n.leading_zeros() - 1)
+    } else {
+        None
+    }
+}
 
 #[cfg(test)]
 mod tests_log2 {
-    use super::log2;
+    use super::{log2, log2_exact};
 
     #[test]
     fn test_log2() {
@@ -62,4 +77,24 @@ mod tests_log2 {
     #[test]
     #[should_panic]
     fn test_log2_0() { log2(0); }
+
+    #[test]
+    fn test_log2_exact() {
+        assert_eq!(log2_exact(1), Some(0));
+        assert_eq!(log2_exact(2), Some(1));
+        assert_eq!(log2_exact(3), None);
+        assert_eq!(log2_exact(4), Some(2));
+        assert_eq!(log2_exact(8), Some(3));
+        assert_eq!(log2_exact(10), None);
+        assert_eq!(log2_exact(63), None);
+        assert_eq!(log2_exact(64), Some(6));
+        assert_eq!(log2_exact(1024), Some(10));
+        assert_eq!(log2_exact(1025), None);
+        assert_eq!(log2_exact(u32::MAX), None);
+    }
+
+    #[test]
+    fn test_log2_0_extact() {
+        assert_eq!(log2_exact(0), None);
+    }
 }
